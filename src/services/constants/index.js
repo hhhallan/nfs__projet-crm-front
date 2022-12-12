7bc8136572245b26390af8a3d9a003b450e3c9ba
hhhallan <allanstm76@gmail.com>
2022-12-12 13:51:19 +0100

    Structure Application

--- src/services/constants/index.js
+++ src/services/constants/index.js
@@ -17,11 +17,21 @@ export const navbar = [
     {
         id: "nav-item-1",
         title: "Home",
-        icon: "icon"
+        icon: "icon-home.png"
     },
     {
         id: "nav-item-2",
-        title: "Section",
-        icon: "icon"
+        title: "Sales",
+        icon: "icon-sales.png"
+    },
+    {
+        id: "nav-item-3",
+        title: "Purchases",
+        icon: "icon-purchases.png"
+    },
+    {
+        id: "nav-item-4",
+        title: "Accounting",
+        icon: "icon-accounting.png"
     },
 ]
