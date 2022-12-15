aaa91ba1f8f4c18c179fbfe5b851045703c07f8a
hhhallan <allanstm76@gmail.com>
2022-12-15 11:38:38 +0100

    Setup Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+// @ts-ignore
+import {Home, About} from "./pages/index";
+import {Navigation} from "./comopnents/index"
+
+
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <BrowserRouter>
+            <header>
+                <Navigation />
+            </header>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
