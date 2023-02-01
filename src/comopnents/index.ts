60f32e0d6baa95da50723862d178e3b52d0bf3b3
hhhallan <allanstm76@gmail.com>
2023-02-01 13:37:25 +0100

    Table components dynamic

--- src/comopnents/index.ts
+++ src/comopnents/index.ts
@@ -1,19 +1,17 @@
 import Navigation from "./Navigation";
 import Button from "./Button";
 import Header from "./Header";
-import TableClient from "./TableClient";
 import StatsCard from "./cards/StatsCard";
 import Chart from "./Chart";
-import TableHistoric from "./TableHistoric";
 import Modal from "./Modal";
+import Table from "./Table";
 
 export {
     Navigation,
     Button,
     Header,
-    TableClient,
     StatsCard,
     Chart,
-    TableHistoric,
     Modal,
+    Table
 }
