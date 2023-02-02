c641bbba6983ef2cbfa25c658462c5e66fde3cba
hhhallan <allanstm76@gmail.com>
2023-02-02 10:52:18 +0100

    Mise en place page Paiement

---
+++ src/pages/Paiement.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Paiement: React.FC = () => {
+    return (
+        <div className="page page-payment">
+            <h5>Paiement</h5>
+            <section className="section">
+                <form action="">
+
+                </form>
+            </section>
+        </div>
+    );
+};
+
+export default Paiement;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,10 +2,12 @@ import Home from "./Home";
 import Client from "./Client";
 import Historic from "./Historic";
 import DevisFactures from "./DevisFactures";
+import Paiement from "./Paiement";
 
 export {
     Home,
     Client,
     Historic,
     DevisFactures,
+    Paiement
 }
